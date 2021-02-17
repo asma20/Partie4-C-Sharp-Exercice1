24a75255e517991a0e401a24a1fa495507bb6763
asma20 <jbeli.asma20@gmail.com>
2021-02-17 16:37:31 +0100

    Créer un tableau,Afficher le contenu de l’indice 1 et 4 du tableau,Afficher le contenu du 1er et du 4ᵉ élément du tableau,Corriger le 4ème jourAjouter le 7ème jour.
    Afficher le tableau dans la console.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Partie4Exercice1nv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String[] jours = { "lundi", "Mardi", "Mercredi", "jedi", "vendredi", "Samedi" };
+
+            Console.WriteLine("le contenu de l'indice 1" + jours[1]);
+            Console.WriteLine("le contenu de l'indice 4" + jours[4]);
+            Console.WriteLine("le contenu  du 1ére élement" + jours[0]);
+            Console.WriteLine("le continu du 4 éme élement" + jours[3]);
+            jours[3] = "jeudi";
+           
+            Array.Resize(ref jours, jours.Length + 1);
+            jours[6] = "dimanche";
+          /*  foreach (String jour in jours)
+            {
+                Console.WriteLine(jour);
+            }
+            
+            */
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine(jours[i].ToString());
+            }
+            
+
+
+
+        }
+    }
+}
